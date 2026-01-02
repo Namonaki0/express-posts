c31992ddb8564ff59967939dcd1808dc2c27ecf4
Andre <andreferreira@live.co.uk>
2026-01-02 23:07:23 +0000

    add delete post logic

--- routes/posts.js
+++ routes/posts.js
@@ -59,4 +59,16 @@ router.put("/:id", (req, res) => {
   res.status(200).json(posts);
 });
 
+// Delete post
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res.status(404).json({ message: `Cannot find post with id ${id}` });
+  }
+  posts = posts.filter((post) => post.id !== id);
+  res.status(200).json(posts);
+});
+
 export default router;
